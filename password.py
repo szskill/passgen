f299865fc9c943c6f9f27c1ef76e4242775ab6cc
Jothin-kumar <bjothinphysics@gmail.com>
2021-11-13 18:03:25 +0530

    Better generate password function

--- password.py
+++ password.py
@@ -1,16 +1,35 @@
-from string import ascii_letters, punctuation, digits
+from string import ascii_lowercase, ascii_uppercase, punctuation, digits
 from random import choice
 
 
-def generate_password(length: int, symbols: bool = False, numbers: int = 12) -> str:
-    char_choices = [*ascii_letters]
-    if symbols:
-        char_choices.extend(punctuation)
-    if numbers:
-        char_choices.extend(digits)
-
+def generate_password(length: int = 8, upper_case: bool = True, lower_case: bool = True,
+                      numbers: bool = True, symbols: bool = True) -> str:
     password = ''
-    for i in range(length):
-        password += choice(char_choices)
+    last_time_character_type = 'symbol'
 
-    return password
+    while len(password) < length:
+        if last_time_character_type == 'symbol':
+            if upper_case:
+                password += choice(ascii_uppercase)
+            last_time_character_type = 'upper case'
+        elif last_time_character_type == 'upper case':
+            if lower_case:
+                password += choice(ascii_lowercase)
+            last_time_character_type = 'lower case'
+        elif last_time_character_type == 'lower case':
+            if numbers:
+                password += choice(digits)
+            last_time_character_type = 'number'
+        elif last_time_character_type == 'number':
+            if symbols:
+                password += choice(punctuation)
+            last_time_character_type = 'symbol'
+
+    new_password = ''
+    password = list(password)
+    while password:
+        random_character = choice(password)
+        new_password += random_character
+        del password[password.index(random_character)]
+
+    return new_password
