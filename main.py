7900b4e1533ab602899df1680ff825f19f281012
Jothin kumar <bjothinphysics@gmail.com>
2021-11-13 18:37:55 +0530

    Update main.py compatibility.

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ parser.add_argument('--length', '-l', type=int, default=12,
                     help='The length of the password to generate.')
 parser.add_argument('--symbols', '-s', default=False, action='store_true',
                     help='Whether to place symbols in the password.')
+parser.add_argument('--upper', '-u', default=False, action='store_true',
+                    help='Whether to use upper case letters in the password.')
+parser.add_argument('--lower', '-l', default=False, action='store_true',
+                    help='Whether to use lower case letters in the password.')
 parser.add_argument('--numbers', '-n', default=False, action='store_true',
                     help='Whether to place numbers in the password.')
 parser.add_argument('--show-weaknesses', '-w', default=False, action='store_true',
@@ -14,7 +18,8 @@ parser.add_argument('--show-weaknesses', '-w', default=False, action='store_true
 
 args = parser.parse_args()
 
-password = generate_password(args.length, symbols=args.symbols, numbers=args.numbers)
+password = generate_password(length=args.length, upper_case=args.upper,
+                             lower_case=args.lower, symbols=args.symbols, numbers=args.numbers)
 print(password)
 
 if args.show_weaknesses:
